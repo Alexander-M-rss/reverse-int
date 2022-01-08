129180710c9665f2b912ecbc2fb55c2ff6d4c4aa
Alexander-M-rss <oleksandr.m.rss@gmail.com>
2022-01-08 19:37:09 +0200

    init: start task reverse init

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
-}
+    let str = n.toString();
+    
+    if (str[0] === '-')
+        str = str.slice(1);
+    
+    return Number(str.split('').reverse().join(''));
+ }
